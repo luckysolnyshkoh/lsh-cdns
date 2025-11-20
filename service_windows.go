20179137d55c217ba31ff954ad2536de4a588327
Solnyshko Lucky H. <lsh@luckysolnyshko.ru>
2025-11-20 18:56:09 +0300

    WIP

--- service_windows.go
+++ service_windows.go
@@ -4,76 +4,76 @@
 package main
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
 
-    "golang.org/x/sys/windows/svc"
+	"golang.org/x/sys/windows/svc"
 )
 
 // Windows Service для Windows
 type windowsService struct{}
 
 func (m *windowsService) Execute(args []string, r <-chan svc.ChangeRequest, changes chan<- svc.Status) (ssec bool, errno uint32) {
-    const cmdsAccepted = svc.AcceptStop | svc.AcceptShutdown
-    changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
+	const cmdsAccepted = svc.AcceptStop | svc.AcceptShutdown
+	changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
 
-    // Инициализируем приложение для сервиса
-    initializeService()
+	// Инициализируем приложение для сервиса
+	initializeService()
 
-    for {
-	select {
-	case c := <-r:
-	    switch c.Cmd {
-	    case svc.Interrogate:
-		changes <- c.CurrentStatus
-	    case svc.Stop, svc.Shutdown:
-		// Корректно останавливаем сервер
-		gracefulShutdown()
-		return false, 0
-	    default:
-		changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
-	    }
+	for {
+		select {
+		case c := <-r:
+			switch c.Cmd {
+			case svc.Interrogate:
+				changes <- c.CurrentStatus
+			case svc.Stop, svc.Shutdown:
+				// Корректно останавливаем сервер
+				gracefulShutdown()
+				return false, 0
+			default:
+				changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
+			}
+		}
 	}
-    }
 }
 
 // initializeService инициализирует приложение для работы как служба
 func initializeService() {
-    log.Printf("[SERVICE] Инициализация службы...")
+	log.Printf("[SERVICE] Инициализация службы...")
 
-    // Определяем ОС
-    OperatingSystem = detectOS()
+	// Определяем ОС
+	OperatingSystem = detectOS()
 
-    // Настраиваем пути
-    setupPaths()
+	// Настраиваем пути
+	setupPaths()
 
-    log.Printf("[SERVICE] ОС: %s", OperatingSystem)
-    log.Printf("[SERVICE] Путь к бинарнику: %s", BinaryPath)
-    log.Printf("[SERVICE] Путь к конфигурации: %s", configPath)
-    log.Printf("[SERVICE] Путь к логам: %s", logsPath)
+	log.Printf("[SERVICE] ОС: %s", OperatingSystem)
+	log.Printf("[SERVICE] Путь к бинарнику: %s", BinaryPath)
+	log.Printf("[SERVICE] Путь к конфигурации: %s", configPath)
+	log.Printf("[SERVICE] Путь к логам: %s", logsPath)
 
-    // Создаем необходимые директории
-    createDirectories()
+	// Создаем необходимые директории
+	createDirectories()
 
-    // Читаем конфигурацию
-    loadConfiguration()
+	// Читаем конфигурацию
+	loadConfiguration()
 
-    // Запускаем DNS сервер
-    log.Printf("[SERVICE] Запуск DNS прокси сервера...")
+	// Запускаем DNS сервер
+	log.Printf("[SERVICE] Запуск DNS прокси сервера...")
 
-    // Запускаем сервер в горутине, чтобы служба не блокировалась
-    go func() {
-	startDnsServer()
-    }()
+	// Запускаем сервер в горутине, чтобы служба не блокировалась
+	go func() {
+		startDnsServer()
+	}()
 
-    log.Printf("[SERVICE] Служба успешно инициализирована")
+	log.Printf("[SERVICE] Служба успешно инициализирована")
 }
 
 // runWindowsService запускает Windows Service
 func runWindowsService() {
-    err := svc.Run("LSHCDNS", &windowsService{})
-    if err != nil {
-	log.Printf("Ошибка запуска службы: %v", err)
-	os.Exit(1)
-    }
+	err := svc.Run("lsh-cdns", &windowsService{})
+	if err != nil {
+		log.Printf("Ошибка запуска службы: %v", err)
+		os.Exit(1)
+	}
 }
